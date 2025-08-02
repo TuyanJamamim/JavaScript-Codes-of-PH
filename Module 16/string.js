0018e3a741bca863f6d3e7739e6601f6af1668ba
TuyanJamamim0599 <tuanjamameem@gmail.com>
2025-08-02 23:37:12 +0600

    second

---
+++ Module 16/string.js
@@ -0,0 +1,18 @@
+// const thana = new String('Derma');
+// console.log(typeof thana);
+// console.log(thana);
+const district = ('Natore');
+console.log( typeof district);
+// console.log(length.district);
+const capital = 'Dha ka';
+console.log(capital.length);//here the length will e 6 ecause space is also counted as character
+console.log(capital[2]);//here the thrird character will e printed
+
+// capital[0] = 'C' ;//unlike array character at 0 index wont e changed....ecause string is immutale and array is mutale;
+
+
+// const numers = [ 1, 2 ,3 , 4, 5, 6, 7, 8, 9];
+// console.log(length.numers);
+// console.log(numers[0]);
+
+
